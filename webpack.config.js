a52f49b141064d4a164b0b67e6948987e350091a
jeffrey <jeffreyye@qq.com>
2022-11-08 21:12:07 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devtool: 'eval',
+    devServer: {
+        contentBase: './dist'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'foo.[contenthash].js',
