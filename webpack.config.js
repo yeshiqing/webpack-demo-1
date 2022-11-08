2989f34006e9a447265e05b4da1d1c3b630e896f
jeffrey <jeffreyye@qq.com>
2022-11-08 20:59:33 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 
@@ -8,4 +9,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'foo.[contenthash].js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Jeffrey Ye',
+            template: 'src/assets/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+
 }
